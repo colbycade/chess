abda737fe5dfe4ada2c45f5ec9b09a518f7f4cad
colbycade <colbycade@gmail.com>
2024-02-26 15:59:48 -0700

    implement create game endpoint

--- server/src/main/java/handler/CreateGameHandler.java
+++ server/src/main/java/handler/CreateGameHandler.java
@@ -1,10 +1,24 @@
 package handler;
 
+import com.google.gson.Gson;
+import dataAccess.AuthDAO;
+import dataAccess.GameDAO;
+import dataAccess.InMemoryDatabase.MemoryAuthDAO;
+import dataAccess.InMemoryDatabase.MemoryGameDAO;
+import exception.DataAccessException;
+import service.GameService;
+import service.request.CreateGameRequest;
+import service.response.CreateGameResponse;
 import spark.Request;
 import spark.Response;
 import spark.Route;
 
 public class CreateGameHandler implements Route {
+    private final GameDAO gameDAO = MemoryGameDAO.getInstance();
+    private final AuthDAO authDAO = MemoryAuthDAO.getInstance();
+    private final GameService gameService = new GameService(gameDAO, authDAO);
+    private final Gson gson = new Gson();
+
     private static CreateGameHandler instance = null;
 
     // Private constructor to prevent direct instantiation
@@ -20,8 +34,11 @@ public class CreateGameHandler implements Route {
     }
 
     @Override
-    public Object handle(Request req, Response res) {
-        // Your handler logic here
-        return null;
+    public Object handle(Request req, Response res) throws DataAccessException {
+        CreateGameRequest createGameRequest = gson.fromJson(req.body(), CreateGameRequest.class);
+        CreateGameResponse createGameResponse = gameService.createGame(createGameRequest);
+        res.status(200);
+        res.type("application/json");
+        return gson.toJson(createGameResponse.gameId());
     }
 }
--- server/src/main/java/service/GameService.java
+++ server/src/main/java/service/GameService.java
@@ -1,4 +1,46 @@
 package service;
 
+import dataAccess.AuthDAO;
+import dataAccess.GameDAO;
+import exception.BadRequestException;
+import exception.DataAccessException;
+import exception.UnauthorizedException;
+import service.request.CreateGameRequest;
+import service.request.JoinGameRequest;
+import service.request.ListGamesRequest;
+import service.response.CreateGameResponse;
+import service.response.ListGamesResponse;
+
 public class GameService {
+    private final GameDAO gameDAO;
+    private final AuthDAO authDAO;
+
+    public GameService(GameDAO gameDAO, AuthDAO authDAO) {
+        this.gameDAO = gameDAO;
+        this.authDAO = authDAO;
+    }
+
+    public CreateGameResponse createGame(CreateGameRequest request) throws DataAccessException {
+        // Verify that the auth token exists
+        if (request.authToken() == null || authDAO.getAuth(request.authToken()) == null) {
+            throw new UnauthorizedException("unauthorized");
+        }
+
+        // Verify that the game name is not null
+        if (request.gameName() == null) {
+            throw new BadRequestException("bad request");
+        }
+
+        // Create game
+        int gameId = gameDAO.createGame(request.authToken());
+        return new CreateGameResponse(gameId);
+    }
+
+    public ListGamesResponse listGames(ListGamesRequest request) throws DataAccessException {
+        return null;
+    }
+
+    public void joinGame(JoinGameRequest request) throws DataAccessException {
+
+    }
 }
