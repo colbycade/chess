1be4215486b42261ed88d18a7cc98f9a64b6a41f
colbycade <colbycade@gmail.com>
2024-02-26 17:59:04 -0700

    unit tests for list games service

--- server/src/test/java/serviceTests/GameServiceTests.java
+++ server/src/test/java/serviceTests/GameServiceTests.java
@@ -1,19 +1,27 @@
 package serviceTests;
 
+import chess.ChessGame;
 import dataAccess.InMemoryDatabase.MemoryAuthDAO;
 import dataAccess.InMemoryDatabase.MemoryGameDAO;
 import exception.BadRequestException;
 import exception.DataAccessException;
 import exception.UnauthorizedException;
+import exception.AlreadyTakenException;
+import model.GameData;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import service.GameService;
 import service.request.CreateGameRequest;
+import service.request.JoinGameRequest;
+import service.request.ListGamesRequest;
 import service.response.CreateGameResponse;
+import service.response.ListGamesResponse;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import java.util.Collection;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class GameServiceTests {
     private GameService gameService;
@@ -53,6 +61,37 @@ public class GameServiceTests {
                     () -> gameService.createGame(new CreateGameRequest(badAuthToken, gameName)));
             assertThrows(BadRequestException.class,
                     () -> gameService.createGame(new CreateGameRequest(authToken, null)));
+        }
+    }
+
+    @Nested
+    class ListGamesTest {
+        String authToken;
+
+        @BeforeEach
+        public void setUp() throws DataAccessException {
+            // Insert a test auth
+            authToken = MemoryAuthDAO.getInstance().createAuth("username").authToken();
+        }
+
+        @Test
+        public void testListGamesSuccess() throws DataAccessException {
+            ListGamesResponse response = gameService.listGames(new ListGamesRequest(authToken));
+            assertNotNull(response);
+            assertNotNull(response.games());
+
+            // Test that a Collection of ChessGames is returned
+            assertInstanceOf(Collection.class, response.games());
+            for (Object game : response.games()) {
+                assertInstanceOf(ChessGame.class, game);
+            }
+        }
+
+        @Test
+        public void testListGamesFailure() {
+            String badAuthToken = "badToken";
+            assertThrows(UnauthorizedException.class,
+                    () -> gameService.listGames(new ListGamesRequest(badAuthToken)));
         }
     }
 }
