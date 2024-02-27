06ca5d7edc0b3a044967d504b343abdf5b7c5bf9
colbycade <colbycade@gmail.com>
2024-02-26 17:58:22 -0700

    implement list games endpoint

--- server/src/main/java/handler/ListGamesHandler.java
+++ server/src/main/java/handler/ListGamesHandler.java
@@ -1,10 +1,24 @@
 package handler;
 
+import com.google.gson.Gson;
+import dataAccess.AuthDAO;
+import dataAccess.GameDAO;
+import dataAccess.InMemoryDatabase.MemoryAuthDAO;
+import dataAccess.InMemoryDatabase.MemoryGameDAO;
+import exception.DataAccessException;
+import service.GameService;
+import service.request.ListGamesRequest;
+import service.response.ListGamesResponse;
 import spark.Request;
 import spark.Response;
 import spark.Route;
 
 public class ListGamesHandler implements Route {
+    private final GameDAO gameDAO = MemoryGameDAO.getInstance();
+    private final AuthDAO authDAO = MemoryAuthDAO.getInstance();
+    private final GameService gameService = new GameService(gameDAO, authDAO);
+    private final Gson gson = new Gson();
+
     private static ListGamesHandler instance = null;
 
     // Private constructor to prevent direct instantiation
@@ -20,8 +34,11 @@ public class ListGamesHandler implements Route {
     }
 
     @Override
-    public Object handle(Request req, Response res) {
-        // Your handler logic here
-        return null;
+    public Object handle(Request req, Response res) throws DataAccessException {
+        ListGamesRequest listGamesRequest = gson.fromJson(req.body(), ListGamesRequest.class);
+        ListGamesResponse listGamesResponse = gameService.listGames(listGamesRequest);
+        res.status(200);
+        res.type("application/json");
+        return gson.toJson(listGamesResponse.games());
     }
 }
--- server/src/main/java/service/response/ListGamesResponse.java
+++ server/src/main/java/service/response/ListGamesResponse.java
@@ -2,7 +2,8 @@ package service.response;
 
 import model.GameData;
 
+import java.util.Collection;
 import java.util.List;
 
-public record ListGamesResponse(List<GameData> games) {
+public record ListGamesResponse(Collection<GameData> games) {
 }
