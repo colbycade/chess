7147de938a600ce88a6ef38f3a5436bc00ce10ea
colbycade <colbycade@gmail.com>
2024-02-26 18:13:04 -0700

    unit tests for join game service

--- server/src/test/java/serviceTests/GameServiceTests.java
+++ server/src/test/java/serviceTests/GameServiceTests.java
@@ -8,7 +8,6 @@ import exception.DataAccessException;
 import exception.UnauthorizedException;
 import exception.AlreadyTakenException;
 import model.GameData;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -94,4 +93,60 @@ public class GameServiceTests {
                     () -> gameService.listGames(new ListGamesRequest(badAuthToken)));
         }
     }
+
+    @Nested
+    class JoinGameTest {
+        String authToken;
+        int gameID;
+        String username = "username";
+        String gameName = "my_game";
+
+
+        @BeforeEach
+        public void setUp() throws DataAccessException {
+            // Insert a test auth
+            authToken = MemoryAuthDAO.getInstance().createAuth(username).authToken();
+            // Create a test game
+            gameID = MemoryGameDAO.getInstance().createGame(gameName);
+        }
+
+        @Test
+        public void testJoinGameSuccess() {
+            // Successful join with white
+            JoinGameRequest successfulRequest = new JoinGameRequest(authToken, ChessGame.TeamColor.WHITE, gameID);
+            assertDoesNotThrow(() -> gameService.joinGame(successfulRequest));
+
+            GameData updatedGame = MemoryGameDAO.getInstance().getGame(gameID);
+            assertEquals(username, updatedGame.whiteUsername());
+            assertNull(updatedGame.blackUsername());
+
+            // Joining as observer doesn't affect the game data
+            JoinGameRequest observerRequest = new JoinGameRequest(authToken, null, gameID);
+            assertDoesNotThrow(() -> gameService.joinGame(observerRequest));
+            assertEquals(updatedGame, MemoryGameDAO.getInstance().getGame(gameID));
+        }
+
+        @Test
+        public void testJoinGameFailure() throws DataAccessException {
+            // Invalid auth token
+            String badAuthToken = "badToken";
+            JoinGameRequest invalidAuthRequest = new JoinGameRequest(badAuthToken, ChessGame.TeamColor.WHITE, gameID);
+            assertThrows(UnauthorizedException.class, () -> gameService.joinGame(invalidAuthRequest));
+
+            // Color already taken
+            // add a game with a white player
+            MemoryGameDAO.getInstance().updateGame(new GameData(gameID, username, null, gameName, new ChessGame()));
+            // insert a second auth
+            String secondAuthToken = MemoryAuthDAO.getInstance().createAuth("secondUsername").authToken();
+            // attempt to join the game with white
+            JoinGameRequest colorTakenRequest = new JoinGameRequest(secondAuthToken, ChessGame.TeamColor.WHITE, gameID);
+            assertThrows(AlreadyTakenException.class, () -> gameService.joinGame(colorTakenRequest));
+
+            // Non-existent game
+            int nonExistentGameID = -1;
+            JoinGameRequest nonExistentGameRequest = new JoinGameRequest(authToken, ChessGame.TeamColor.WHITE, nonExistentGameID);
+            assertThrows(BadRequestException.class, () -> gameService.joinGame(nonExistentGameRequest));
+        }
+    }
+
 }
