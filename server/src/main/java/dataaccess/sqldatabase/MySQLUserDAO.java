daba7ad725089bf0ddc06c8fb5940515bc0ffc32
colbycade <colbycade@gmail.com>
2024-02-29 23:08:19 -0700

    implement MySQLUserDAO

--- server/src/main/java/dataaccess/sqldatabase/MySQLUserDAO.java
+++ server/src/main/java/dataaccess/sqldatabase/MySQLUserDAO.java
@@ -11,19 +11,69 @@ import static java.sql.Statement.RETURN_GENERATED_KEYS;
 
 public class MySQLUserDAO implements UserDAO {
 
+    public MySQLUserDAO() throws DataAccessException {
+        DatabaseManager.createDatabase();
+        try (var conn = DatabaseManager.getConnection()) {
+            var statement = """
+                    CREATE TABLE IF NOT EXISTS User (
+                        username VARCHAR(64) PRIMARY KEY,
+                        password VARCHAR(64) NOT NULL,
+                        email VARCHAR(330) NOT NULL
+                    )""";
+            try (var preparedStatement = conn.prepareStatement(statement)) {
+                preparedStatement.executeUpdate();
+            }
+        } catch (SQLException e) {
+            throw new DataAccessException(e.getMessage());
+        }
+    }
 
     @Override
     public void insertUser(UserData user) throws DataAccessException {
-
+        try (var conn = DatabaseManager.getConnection()) {
+            var statement = "INSERT INTO User (username, password, email) VALUES (?, ?, ?)";
+            try (var preparedStatement = conn.prepareStatement(statement, RETURN_GENERATED_KEYS)) {
+                preparedStatement.setString(1, user.username());
+                preparedStatement.setString(2, user.password());
+                preparedStatement.setString(3, user.email());
+                preparedStatement.executeUpdate();
+            }
+        } catch (SQLException e) {
+            throw new DataAccessException(e.getMessage());
+        }
     }
 
     @Override
     public UserData getUser(String username) throws DataAccessException {
-        return null;
+        try (var conn = DatabaseManager.getConnection()) {
+            var statement = "SELECT * FROM User WHERE username = ?";
+            try (var preparedStatement = conn.prepareStatement(statement)) {
+                preparedStatement.setString(1, username);
+                var resultSet = preparedStatement.executeQuery();
+                if (resultSet.next()) {
+                    return new UserData(
+                            resultSet.getString("username"),
+                            resultSet.getString("password"),
+                            resultSet.getString("email")
+                    );
+                } else {
+                    throw new DataAccessException("User not found");
+                }
+            }
+        } catch (SQLException e) {
+            throw new DataAccessException(e.getMessage());
+        }
     }
 
     @Override
     public void clear() throws DataAccessException {
-
+        try (var conn = DatabaseManager.getConnection()) {
+            var statement = "TRUNCATE TABLE User";
+            try (var preparedStatement = conn.prepareStatement(statement)) {
+                preparedStatement.executeUpdate();
+            }
+        } catch (SQLException e) {
+            throw new DataAccessException(e.getMessage());
+        }
     }
 }
