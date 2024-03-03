13ab320acf82808b6dcdd65a9786fbde2a8c5ced
colbycade <colbycade@gmail.com>
2024-03-02 17:22:12 -0700

    add tests for MySQLGameDAO

---
+++ server/src/test/java/dataaccesstests/GameDAOTests.java
@@ -0,0 +1,84 @@
+package dataaccesstests;
+
+import chess.ChessGame;
+import chess.ChessMove;
+import chess.ChessPosition;
+import chess.InvalidMoveException;
+import dataaccess.GameDAO;
+import dataaccess.inmemorydatabase.MemoryGameDAO;
+import dataaccess.sqldatabase.MySQLGameDAO;
+import exception.DataAccessException;
+import model.GameData;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Collection;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class GameDAOTests {
+
+    private GameDAO gameDAO;
+
+    @BeforeEach
+    public void setUp() throws DataAccessException {
+        // Default to MemoryUserDAO if the property is not set
+        // (edit run config or use -DuserDaoType=mysql in VM options)
+        String daoType = System.getProperty("userDaoType", "memory");
+        switch (daoType) {
+            case "mysql" -> gameDAO = new MySQLGameDAO();
+            case "memory" -> gameDAO = MemoryGameDAO.getInstance();
+        }
+        gameDAO.clear();
+    }
+
+    @Test
+    public void testCreateGame() throws DataAccessException {
+        int gameID = gameDAO.createGame("testGame");
+        assertNotNull(gameID);
+    }
+
+    @Test
+    public void testGetGame() throws DataAccessException {
+        int gameID = gameDAO.createGame("testGame");
+        GameData game = gameDAO.getGame(gameID);
+        assertNotNull(game);
+        System.out.println(game);
+    }
+
+    @Test
+    public void testUpdateGame() throws DataAccessException, InvalidMoveException {
+        int gameID = gameDAO.createGame("testGame");
+        GameData oldGame = gameDAO.getGame(gameID);
+        ChessGame testGame = oldGame.game();
+        ChessMove testMove = new ChessMove(new ChessPosition(2, 1), new ChessPosition(3, 1), null);
+        testGame.makeMove(testMove);
+        GameData updatedGame = new GameData(gameID, "newWhiteUser", "newBlackUser", "newName", testGame);
+        gameDAO.updateGame(updatedGame);
+        GameData returnedGame = gameDAO.getGame(gameID);
+        assertEquals("newName", returnedGame.gameName());
+    }
+
+    @Test
+    public void testClear() throws DataAccessException {
+        gameDAO.createGame("testGame1");
+        gameDAO.createGame("testGame2");
+        gameDAO.createGame("testGame3");
+        gameDAO.createGame("testGame4");
+        gameDAO.clear();
+        Collection<GameData> games = gameDAO.listGames();
+        assertNotNull(games);
+        assertEquals(0, games.size());
+    }
+
+    @Test
+    public void testListGames() throws DataAccessException {
+        gameDAO.createGame("testGame1");
+        gameDAO.createGame("testGame2");
+        gameDAO.createGame("testGame3");
+        gameDAO.createGame("testGame4");
+        Collection<GameData> games = gameDAO.listGames();
+        assertNotNull(games);
+        assertEquals(4, games.size());
+    }
+}
