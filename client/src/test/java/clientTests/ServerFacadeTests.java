db21a4c53ef897532b5714bf45361c5cd89d1533
colbycade <colbycade@gmail.com>
2024-03-19 21:01:54 -0600

    add unit tests for register

--- client/src/test/java/clientTests/ServerFacadeTests.java
+++ client/src/test/java/clientTests/ServerFacadeTests.java
@@ -1,21 +1,34 @@
 package clientTests;
 
+import dataAccess.inMemoryDatabase.MemoryAuthDAO;
+import dataAccess.inMemoryDatabase.MemoryGameDAO;
+import dataAccess.inMemoryDatabase.MemoryUserDAO;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import server.Server;
+import ui.ResponseException;
+import ui.ServerFacade;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class ServerFacadeTests {
 
     private static Server server;
+    static ServerFacade facade;
 
     @BeforeAll
     public static void init() {
-        server = new Server();
-        var port = server.run(0);
+        // Start server using in-memory database for testing
+        MemoryAuthDAO authDAO = MemoryAuthDAO.getInstance();
+        MemoryGameDAO gameDAO = MemoryGameDAO.getInstance();
+        MemoryUserDAO userDAO = MemoryUserDAO.getInstance();
+        server = new Server(authDAO, gameDAO, userDAO);
+        int port = server.run(8080);
         System.out.println("Started test HTTP server on " + port);
+        facade = new ServerFacade(port);
     }
 
     @AfterAll
@@ -23,9 +36,24 @@ public class ServerFacadeTests {
         server.stop();
     }
 
+    @BeforeEach
+    void clearData() {
+        MemoryAuthDAO.getInstance().clear();
+        MemoryGameDAO.getInstance().clear();
+        MemoryUserDAO.getInstance().clear();
+    }
 
     @Test
-    public void sampleTest() {
-        assertTrue(true);
+    void registerSuccess() {
+        assertDoesNotThrow(() -> facade.register("player1", "password", "p1@email.com"));
+    }
+
+    @Test
+    void registerFail() throws Exception {
+        // test invalid inputs throws exception
+        assertThrows(ResponseException.class, () -> facade.register(null, null, null));
+        // test duplicate username throws exception
+        facade.register("player1", "password", "p1@email.com");
+        assertThrows(ResponseException.class, () -> facade.register("player1", "password", "p1@email.com"));
     }
 }
