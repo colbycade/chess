d83ee0bb6e64341d6199662140da5f7c5ae61466
colbycade <colbycade@gmail.com>
2024-03-20 12:12:07 -0600

    add unit tests for login

--- client/src/test/java/clientTests/ServerFacadeTests.java
+++ client/src/test/java/clientTests/ServerFacadeTests.java
@@ -3,6 +3,7 @@ package clientTests;
 import dataAccess.inMemoryDatabase.MemoryAuthDAO;
 import dataAccess.inMemoryDatabase.MemoryGameDAO;
 import dataAccess.inMemoryDatabase.MemoryUserDAO;
+import model.UserData;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -11,21 +12,21 @@ import server.Server;
 import ui.ResponseException;
 import ui.ServerFacade;
 
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ServerFacadeTests {
 
     private static Server server;
     static ServerFacade facade;
 
+    MemoryAuthDAO authDAO = MemoryAuthDAO.getInstance();
+    MemoryGameDAO gameDAO = MemoryGameDAO.getInstance();
+    MemoryUserDAO userDAO = MemoryUserDAO.getInstance();
+
     @BeforeAll
     public static void init() {
         // Start server using in-memory database for testing
-        MemoryAuthDAO authDAO = MemoryAuthDAO.getInstance();
-        MemoryGameDAO gameDAO = MemoryGameDAO.getInstance();
-        MemoryUserDAO userDAO = MemoryUserDAO.getInstance();
-        server = new Server(authDAO, gameDAO, userDAO);
+        server = new Server(MemoryAuthDAO.getInstance(), MemoryGameDAO.getInstance(), MemoryUserDAO.getInstance());
         int port = server.run(8080);
         System.out.println("Started test HTTP server on " + port);
         facade = new ServerFacade(port);
@@ -38,22 +39,39 @@ public class ServerFacadeTests {
 
     @BeforeEach
     void clearData() {
-        MemoryAuthDAO.getInstance().clear();
-        MemoryGameDAO.getInstance().clear();
-        MemoryUserDAO.getInstance().clear();
+        authDAO.clear();
+        gameDAO.clear();
+        userDAO.clear();
     }
 
     @Test
     void registerSuccess() {
         assertDoesNotThrow(() -> facade.register("player1", "password", "p1@email.com"));
+        assertNotNull(facade.getAuthToken());
     }
 
     @Test
     void registerFail() throws Exception {
-        // test invalid inputs throws exception
+        // Invalid inputs throws exception
         assertThrows(ResponseException.class, () -> facade.register(null, null, null));
-        // test duplicate username throws exception
+        // Duplicate username throws exception
         facade.register("player1", "password", "p1@email.com");
         assertThrows(ResponseException.class, () -> facade.register("player1", "password", "p1@email.com"));
+    }
+
+    @Test
+    void loginSuccess() throws Exception {
+        // Insert test user into database
+        userDAO.insertUser(new UserData("player1", "password", "p1@email.com"));
+
+        // Login successful
+        assertDoesNotThrow(() -> facade.login("player1", "password"));
+        assertNotNull(facade.getAuthToken());
+    }
+
+    @Test
+    void loginFail() throws Exception {
+        // Login unregistered user throws exception
+        assertThrows(ResponseException.class, () -> facade.login("unknown_player", "wrong_password"));
     }
 }
