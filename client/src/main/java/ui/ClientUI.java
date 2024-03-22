89289e951471ae1447c4191acf71301578180705
colbycade <colbycade@gmail.com>
2024-03-21 20:11:21 -0600

    add chess board display both perspectives only

--- client/src/main/java/ui/ClientUI.java
+++ client/src/main/java/ui/ClientUI.java
@@ -20,26 +20,40 @@ public class ClientUI {
         scanner = new Scanner(System.in);
     }
 
-    public static String displayBoard(ChessBoard board) {
+    public static String displayBoard(ChessBoard board, ChessGame.TeamColor perspective) {
         StringBuilder boardDisplay = new StringBuilder();
         boardDisplay.append(ERASE_SCREEN);
         boardDisplay.append(SET_TEXT_BOLD + SET_TEXT_COLOR_BLUE + SET_BG_COLOR_WHITE);
-        String columnLabels = "    a  b  c  d  e  f  g  h    " + RESET_ALL + "\n";
-        boardDisplay.append(columnLabels);
-        for (int row = 8; row >= 1; row--) {
+
+        String columnLabels = "    a  b  c  d  e  f  g  h    " + RESET_ALL;
+        boardDisplay.append(columnLabels).append("\n");
+
+        int rowStart = perspective == ChessGame.TeamColor.WHITE ? 8 : 1;
+        int rowEnd = perspective == ChessGame.TeamColor.WHITE ? 1 : 8;
+        int rowDirection = perspective == ChessGame.TeamColor.WHITE ? -1 : 1;
+
+        int colStart = perspective == ChessGame.TeamColor.WHITE ? 1 : 8;
+        int colEnd = perspective == ChessGame.TeamColor.WHITE ? 8 : 1;
+        int colDirection = perspective == ChessGame.TeamColor.WHITE ? 1 : -1;
+
+        for (int row = rowStart; row != rowEnd + rowDirection; row += rowDirection) {
             boardDisplay.append(SET_TEXT_BOLD + SET_TEXT_COLOR_RED + SET_BG_COLOR_WHITE);
             boardDisplay.append(" ").append(row).append(" ").append(RESET_ALL).append(SET_TEXT_COLOR_BLACK);
-            for (int col = 1; col <= 8; col++) {
+
+            for (int col = colStart; col != colEnd + colDirection; col += colDirection) {
                 ChessPiece piece = board.getPiece(new ChessPosition(row, col));
                 String pieceString = getString(piece);
                 boardDisplay.append(SET_TEXT_BOLD).append((row + col) % 2 == 0 ? SET_BG_COLOR_LIGHT_GREY : SET_BG_COLOR_DARK_GREY);
                 boardDisplay.append(pieceString);
             }
+
             boardDisplay.append(SET_TEXT_BOLD + SET_TEXT_COLOR_RED + SET_BG_COLOR_WHITE);
             boardDisplay.append(" ").append(row).append(" ").append(RESET_ALL).append("\n");
         }
+
         boardDisplay.append(SET_TEXT_BOLD + SET_TEXT_COLOR_BLUE + SET_BG_COLOR_WHITE);
         boardDisplay.append(columnLabels);
+
         return boardDisplay.toString();
     }
 
@@ -146,7 +160,10 @@ public class ClientUI {
                             count++;
                             System.out.println(SET_TEXT_COLOR_MAGENTA + " Game " + SET_TEXT_COLOR_YELLOW + count + SET_TEXT_COLOR_MAGENTA + ": " + RESET_ALL);
                             ChessBoard board = game.game().getBoard();
-                            System.out.println(displayBoard(board));
+                            // Display board from both perspectives
+                            System.out.println(displayBoard(board, ChessGame.TeamColor.WHITE));
+                            System.out.println(SET_BG_COLOR_BLACK + " ".repeat(30) + RESET_ALL);
+                            System.out.println(displayBoard(board, ChessGame.TeamColor.BLACK));
                         }
                     } catch (ResponseException e) {
                         System.out.println(SET_TEXT_COLOR_RED + "Failed to retrieve games.");
