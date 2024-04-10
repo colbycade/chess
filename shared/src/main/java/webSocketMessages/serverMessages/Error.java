306a354c0eb78873a3e8f9fdb8087d382d214701
colbycade <colbycade@gmail.com>
2024-04-09 20:24:46 -0600

    create Server Message classes

---
+++ shared/src/main/java/webSocketMessages/serverMessages/Error.java
@@ -0,0 +1,10 @@
+package webSocketMessages.serverMessages;
+
+public class Error extends ServerMessage {
+    private final String errorMessage;
+    
+    public Error(String message) {
+        super(ServerMessageType.ERROR);
+        this.errorMessage = message;
+    }
+}
---
+++ shared/src/main/java/webSocketMessages/serverMessages/LoadGame.java
@@ -0,0 +1,13 @@
+package webSocketMessages.serverMessages;
+
+import model.GameData;
+
+public class LoadGame extends ServerMessage {
+    
+    private final GameData game;
+    
+    public LoadGame(GameData game) {
+        super(ServerMessageType.LOAD_GAME);
+        this.game = game;
+    }
+}
